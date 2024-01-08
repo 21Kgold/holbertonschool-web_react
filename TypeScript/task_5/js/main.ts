c2d17cf14d52f0a921442eb208111c37626b184c
cau_rpa <claudiarpa@gmail.com>
2024-01-08 03:08:11 -0600

    task_5

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+export interface MajorCredits {
+    credits: number;
+    brandMajor: void;
+}
+
+export interface MinorCredits {
+    credits: number;
+    brandMinor: void;
+}
+
+export const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits): MajorCredits => {
+    return {credits: subject1.credits + subject2.credits} as MajorCredits;
+}
+
+export const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): MinorCredits => {
+    return {credits: subject1.credits + subject2.credits} as MinorCredits;
+}
