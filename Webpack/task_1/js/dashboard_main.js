7467aa849534606f6b57181e0748a4e854fd0936
cau_rpa <claudiarpa@gmail.com>
2024-01-23 19:15:45 -0600

    task_1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+import _ from "lodash";
+
+let count = 0;
+
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button type="button" id="startButton">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    // Use event delegation to handle the click event
+    $('body').on('click', '#startButton', _.debounce(updateCounter, 500));
+});
+
