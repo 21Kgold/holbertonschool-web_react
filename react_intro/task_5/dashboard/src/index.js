d1d10aa2c2331f06e6378f9c1e7961131eef4ab5
cau_rpa <claudiarpa@gmail.com>
2024-01-25 03:13:34 -0600

    task_5 Testing with Enzyme and Jest

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-import Notifications from './Notifications';
+import Notifications from './Notifications/Notifications';
 
 ReactDOM.render(
   <React.StrictMode>
