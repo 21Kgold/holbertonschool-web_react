d18f723bab49b06bc2d4ed57c2db3edadbe85196
cau_rpa <claudiarpa@gmail.com>
2024-01-25 01:53:47 -0600

    task_5 Reorganize the files

---
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Notifications.css';
+import icon from '../assets/icons8-close-30.png';
+import {getLatestNotification} from '../utils/utils';
+
+function Notifications() {
+    const handleClick = () => {
+        console.log('Close button has been clicked');
+    }
+    return (
+        <div className="Notifications">
+            <button aria-label='Close' onClick={handleClick} style={{float: 'right', backgroundColor: 'transparent', border: 'none'}}>
+                <img src={icon} alt='icon' style={{backgroundColor: 'transparent', border: 'none'}}></img>
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Notifications;
