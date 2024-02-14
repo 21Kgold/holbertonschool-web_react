0c95398f08841575f1fa4ac70d1d737fef3abe1f
Claudia <claudiarpa@gmail.com>
2024-02-13 21:48:47 -0600

    task_1

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import BodySection from './BodySection';
+import {StyleSheet, css} from 'aphrodite';
 
+
+
+const styles = StyleSheet.create({
+    bodySectionWithMargin: {
+        marginBottom: '40px'
+    }
+})
 
 function BodySectionWithMarginBottom(props) {
     return (
-        <div>
+        <div className={css(styles.bodySectionWithMargin)}>
             <BodySection {...props}></BodySection>
         </div>
     );
