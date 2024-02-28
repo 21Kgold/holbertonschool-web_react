55822097a8095d9e1ee69076fdbaa559390a06cc
Claudia <claudiarpa@gmail.com>
2024-02-28 03:26:37 -0600

    task_6

---
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,21 @@
+import {MARK_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters} from './notificationActionTypes'
+
+export const markAsread = (index) => {
+    return {
+        type: MARK_AS_READ,
+        index
+      }
+};
+
+export const setNotificationFilter = (filter) => {
+    if (filter === NotificationTypeFilters[1]) {
+        return { type: SET_TYPE_FILTER, filter }
+    } else {
+        return { type: SET_TYPE_FILTER, filter: NotificationTypeFilters[0] }
+    }   
+};
+
+// Manually binding action creators to dispatch
+export const boundMarkAsread = (index) => dispatch(markAsread(index));
+export const boundSetNotificationFilter = (filter) => dispatch(setNotificationFilter(filter));
+
