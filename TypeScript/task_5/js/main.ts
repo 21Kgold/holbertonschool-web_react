72c7180480f359dbecbf9791dd706fc0e21dc027
claudia <claudiarpa@gmail.com>
2024-08-15 04:39:34 -0600

    console log of constant

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -15,3 +15,7 @@ export const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits):
 export const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): MinorCredits => {
     return {credits: subject1.credits + subject2.credits} as MinorCredits;
 }
+
+const subject3: MajorCredits = {credits: 3} as MajorCredits;
+const example = sumMajorCredits(subject3, subject3);
+console.log(example);
