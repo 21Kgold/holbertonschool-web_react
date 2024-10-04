496fcf63a745ec599570c83120a9e7e3b7e3615f
claudia <claudiarpa@gmail.com>
2024-10-04 00:07:26 -0600

    style update

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-import reportWebVitals from './reportWebVitals';
 import Notifications from './Notifications/Notifications';
 
 ReactDOM.render(
@@ -13,5 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
