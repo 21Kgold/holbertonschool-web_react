d20a512bff682f66abc6471697011ec739b85473
claudia <claudiarpa@gmail.com>
2024-11-16 20:55:04 -0600

    style: Add code guidelines

---
+++ react_props/task_2/dashboard/src/App/App.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import App from "./App"
+
+test('App renders without crashing', () => {
+    shallow(<App />);
+});
+
+test('Verify that App renders a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Notifications').length).toBe(1);
+});
+
+test('Verify that App renders a div with the class App-header', () => {
+    const wrapper = shallow(<App>
+        <div className="App"></div>
+    </App>);
+    expect(wrapper.find('Header').length).toBe(1);
+});
+
+test('Verify that App renders a div with the class App-header', () => {
+    const wrapper = shallow(<App>
+        <div className="App"></div>
+    </App>);
+    expect(wrapper.find('Login').length).toBe(1);
+});
+
+test('Verify that App renders a div with the class App-header', () => {
+    const wrapper = shallow(<App>
+        <div className="App"></div>
+    </App>);
+    expect(wrapper.find('Footer').length).toBe(1);
+});
---
+++ react_props/task_2/dashboard/src/Header/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Header from './Header';
+
+test('Header component renders whitout crashing', () => {
+    shallow(<Header />);
+});
+
+test('Verify that the Header component render (1) img and (1) h1 tags', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('img').length).toBe(1);
+    expect(wrapper.find('h1').length).toBe(1);
+});
